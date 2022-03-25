e604f2961fc564d43b5a439c8f78e35b7077296f
tapia1013 <manbearpig1013@gmail.com>
2022-03-24 20:58:47 -0700

    get user login info from firebase and show in profile

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,9 +1,19 @@
+import { useState, useEffect } from 'react'
+import { getAuth } from 'firebase/auth'
+
+
 
 function Profile() {
+  const [user, setUser] = useState(null)
+
+  const auth = getAuth();
+
+  useEffect(() => {
+    setUser(auth.currentUser);
+  }, [])
+
   return (
-    <div>
-      <h1>Profile</h1>
-    </div>
+    user ? <h1>{user.displayName}</h1> : 'Not Logged In'
   )
 }
 
