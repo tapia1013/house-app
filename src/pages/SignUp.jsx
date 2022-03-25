f650b00cfd274773c66f341bfe352ef1c98238c2
tapia1013 <manbearpig1013@gmail.com>
2022-03-24 20:21:50 -0700

    signup signup authentication and updateprofile and redirect

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import { getAuth, createUserWithEmailAndPassword, updateProfile } from 'firebase/auth';
+import { db } from '../firebase.config';
 import { ReactComponent as ArrowRightIcon } from '../assets/svg/keyboardArrowRightIcon.svg'
 import visibilityIcon from '../assets/svg/visibilityIcon.svg'
 
@@ -27,6 +29,29 @@ function SignUp() {
     }))
   }
 
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const auth = getAuth();
+
+      const userCredential = await createUserWithEmailAndPassword(auth, email, password)
+
+      const user = userCredential.user;
+
+      updateProfile(auth.currentUser, {
+        displayName: name
+      })
+
+      navigate('/')
+    } catch (error) {
+      console.log(error)
+    }
+
+  }
+
+
   return (
     <>
       <div className="pageContainer">
@@ -36,7 +61,7 @@ function SignUp() {
           </p>
         </header>
 
-        <form>
+        <form onSubmit={onSubmit}>
           <input
             placeholder='Name'
             type="text"
