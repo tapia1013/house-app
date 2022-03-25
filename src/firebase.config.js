29187d0c17e5123ab7b3df8e97925f8db41cd8a5
tapia1013 <manbearpig1013@gmail.com>
2022-03-24 17:56:38 -0700

    first commit

---
+++ src/firebase.config.js
@@ -0,0 +1,21 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from 'firebase/firestore'
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAeLr-j-7JdHFRdBtzL75Z9ScnkwEk5luI",
+  authDomain: "house-marketplace-app-ca10e.firebaseapp.com",
+  projectId: "house-marketplace-app-ca10e",
+  storageBucket: "house-marketplace-app-ca10e.appspot.com",
+  messagingSenderId: "922893244593",
+  appId: "1:922893244593:web:1e95bb668ec705a2272a90",
+  measurementId: "G-R54DK8CCFF"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+export const db = getFirestore();
